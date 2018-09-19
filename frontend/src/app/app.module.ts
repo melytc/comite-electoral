d4fdef035366c4a32c77e2219cdcbf182355bd6d
miguelc95 <miguelc.95@hotmail.com>
2018-09-19 15:04:01 -0500

    login and router outlet added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
+
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material';
@@ -17,14 +19,23 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SignupComponent } from './signup/signup.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component:LoginComponent },
+  { path: 'signup', component: SignupComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NavBarComponent
+    NavBarComponent,
+    SignupComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     AngularFireModule.initializeApp(environment.config),
     AngularFireAuthModule,
--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth'
-
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-login',
@@ -11,13 +11,18 @@ export class LoginComponent implements OnInit {
   email : string = "";
   password : string = "";
 
-  constructor(public auth: AngularFireAuth) { }
+  constructor(public auth: AngularFireAuth, private router : Router) { }
 
   ngOnInit() {
   }
 
   login(){
-    console.log(this.email)
+    this.auth.auth.signInWithEmailAndPassword(this.email,this.password).then((user)=>{
+      console.log(user)
+      //this.router.navigateByUrl('home');
+    }).catch((error)=>{
+      alert(error);
+    })
   }
 
 }
