861e649cf6a48048681329b621de8599f80747c3
Carmelo <jcarmelo37@gmail.com>
2018-10-04 23:37:35 -0500

    Sign Up Page
    Sign up page added with a sign up form

--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from "@angular/fire/auth";
 import { AngularFireDatabase } from "@angular/fire/database";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -9,29 +10,35 @@ import { AngularFireDatabase } from "@angular/fire/database";
 })
 export class SignupComponent implements OnInit {
   email : string;
-  password :string ;
+  password :string;
+  confPassword : string;
   matricula : string;
 
-  constructor(private auth : AngularFireAuth, private db : AngularFireDatabase) { }
+  constructor(private auth : AngularFireAuth, private db : AngularFireDatabase, private router: Router) { }
 
   ngOnInit() {
    
   }
 
   signUp(){
-    this.auth.auth.createUserWithEmailAndPassword('','').then((user)=>{
-      this.db.database.ref(`userId/${user.user.uid}`).set({
-        matricula : this.matricula
-      })
-      //Obtener datos de la base de datos de los registrados candidatos
-      this.db.database.ref(`database/${this.matricula}/`).once('value').then(snap=>{
-        //setear info de nuevo candidato
-        this.db.database.ref(`registrados/${this.matricula}`).set({
-          estado : snap.val().state,
-          uid : user.user.uid,
+    if(this.password === this.confPassword) {
+      this.auth.auth.createUserWithEmailAndPassword(this.email,this.password).then((user)=>{
+        this.db.database.ref(`userId/${user.user.uid}`).set({
+          matricula : this.matricula
+        })
+        //Obtener datos de la base de datos de los registrados candidatos
+        this.db.database.ref(`database/${this.matricula}/`).once('value').then(snap=>{
+          //setear info de nuevo candidato
+          this.db.database.ref(`registrados/${this.matricula}`).set({
+            estado : snap.val().state,
+            uid : user.user.uid,
+          })
+          this.router.navigateByUrl('/admin/home');
         })
       })
-    })
+    } else {
+      console.log('Password not confirmed.')
+    }
   }
 
 }
